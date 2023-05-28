b650cd695c09548a73952e579ebdd7d50b462a58
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-05-29 03:28:09 +0800

    M

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: , res: Response) => {
   res.send('hello world Vai')
 })
