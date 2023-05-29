19c6f640b4baa4a08c84bf08d83f6e926d01523a
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-05-29 11:01:58 +0800

    testing after using yarn

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 export const app: Application = express()
+//cors
 app.use(cors())
 
 //parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: , res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('hello world Vai')
 })
