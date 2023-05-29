88303f9ac76955a2138b0c69aec077e9b40531d9
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-05-29 11:10:06 +0800

    mmm

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import cors from 'cors'
 export const app: Application = express()
 //cors
 app.use(cors())
+//Hello world
 
 //parser
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
   res.send('hello world Vai')
